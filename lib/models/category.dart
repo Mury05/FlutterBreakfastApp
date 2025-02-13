b66f92b6ac1ff3a3122a2ac07305ff9ce0142452
Mury05 <murielleodilone@gmail.com>
2025-02-13 15:31:47 +0100

    Quelques modifications dans le model category

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,11 +1,13 @@
 class Category {
-  final String name; 
+  final String name;
   final String iconPath;
 
   Category({required this.name, required this.iconPath});
 }
+
 List<Category> categories = [
   Category(name: 'Salad', iconPath: 'assets/icons/plate.svg'),
   Category(name: 'Cake', iconPath: 'assets/icons/pancakes.svg'),
   Category(name: 'Pie', iconPath: 'assets/icons/pie.svg'),
+  Category(name: 'Snacks', iconPath: 'assets/icons/orange-snacks.svg'),
 ];
